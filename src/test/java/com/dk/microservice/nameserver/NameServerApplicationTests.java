96fbd71f57f44e6e66aab250aa6827f500315ae6
Adnan Rahman <adnanrahman@adnans-mbp.myfiosgateway.com>
2021-08-26 00:46:34 -0400

    naming server for microservices

---
+++ src/test/java/com/dk/microservice/nameserver/NameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.microservice.nameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
